06962e8f6cc6973caa077f74144c263bd7743543
memo <mohamed.ibrahim.alean@gmail.com>
2024-12-25 09:14:14 +0200

    Adding folder structure for the project

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color primaryColor = const Color(0xffE2BE7F);
+  static Color secondaryColor = const Color(0xff202020);
+  static Color white = const Color(0xffFFFFFF);
+}
