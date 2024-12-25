0c428d0d9321d8103ea44d70cfec1eeb619ca9d8
memo <mohamed.ibrahim.alean@gmail.com>
2024-12-25 11:44:56 +0200

    finish splash page

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,17 @@
+class AppAssets {
+  static String splashBG = "assets/images/splash_bg.png";
+  static String splashGlow = "assets/images/splash_glow.png";
+  static String splashLeftShape = "assets/images/splash_left_shape.png";
+  static String splashRightShape = "assets/images/splash_right_shape.png";
+  static String splashLogo = "assets/images/splash_logo.png";
+  static String islamLogo = "assets/images/islam.png";
+  static String supervisedText = "assets/images/supervised_text.png";
+  static String splashBrand = "assets/images/splash_brand.png";
+  static String quranBg = "assets/images/quran_bg.png";
+  static String quranIcn = "assets/icons/quran.png";
+  static String hadithIcn = "assets/icons/hadith.png";
+  static String sbhaIcn = "assets/icons/sbha.png";
+  static String radioIcn = "assets/icons/radio.png";
+  static String timesIcn = "assets/icons/times.png";
+  static String hadithBG = "assets/images/hadith_bg.png";
+}
---
+++ lib/features/splash/pages/splash_page.dart
@@ -0,0 +1,124 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:islami_app/core/constants/app_assets.dart';
+
+class SplashPage extends StatefulWidget {
+  static String routeName = "splash";
+  static final Duration _duration = Duration(
+    milliseconds: 1750,
+  );
+
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+            AppAssets.splashBG,
+          ),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: FadeInDown(
+                duration: SplashPage._duration,
+                child: Image.asset(
+                  AppAssets.splashGlow,
+                  height: size.height * 0.30,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: ZoomIn(
+                duration: SplashPage._duration,
+                child: Image.asset(
+                  AppAssets.splashLogo,
+                  width: size.width * 0.50,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: FadeInUp(
+                duration: SplashPage._duration,
+                child: Image.asset(
+                  AppAssets.splashBrand,
+                  height: size.height * 0.25,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: FadeInUp(
+                duration: SplashPage._duration,
+                child: Image.asset(
+                  AppAssets.supervisedText,
+                  height: size.height * 0.14,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: size.height * 0.28),
+              child: Align(
+                alignment: Alignment.center,
+                child: ZoomIn(
+                  duration: SplashPage._duration,
+                  child: Image.asset(
+                    AppAssets.islamLogo,
+                    width: size.width * 0.40,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: size.height * 0.25),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: FadeInLeft(
+                  duration: SplashPage._duration,
+                  child: Image.asset(
+                    AppAssets.splashLeftShape,
+                    width: size.width * 0.25,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: size.height * 0.25),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: FadeInRight(
+                  duration: SplashPage._duration,
+                  child: Image.asset(
+                    AppAssets.splashRightShape,
+                    width: size.width * 0.25,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
